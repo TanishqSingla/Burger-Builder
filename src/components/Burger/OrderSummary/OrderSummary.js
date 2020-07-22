d34c428b07d4a6690a6f8eed9b8694fcfb93858f
TanishqSingla <tanishqsingla00@gmail.com>
2020-07-23 02:51:30 +0530

    Optimized order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -2,30 +2,34 @@ import React from "react";
 
 import Button from "./../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients).map((igKey) => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
-        {props.ingredients[igKey]}
-      </li>
+class OrderSummary extends React.Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (igKey) => {
+        return (
+          <li key={igKey}>
+            <span style={{ textTransform: "capitalize" }}>{igKey}</span>:{" "}
+            {this.props.ingredients[igKey]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>Your preferred burger with following ingredients: </p>
-      <ul>{ingredientSummary}</ul>
-      <p>Total Price: ${props.price}</p>
-      <p>Continue Checkout?</p>
-      <Button clicked={props.purchaseCancelled} btnType={`Danger`}>
-        Cancel
-      </Button>
-      <Button btnType={`Success`} clicked={props.purchaseContinued}>
-        Continue
-      </Button>
-    </>
-  );
-};
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>Your preferred burger with following ingredients: </p>
+        <ul>{ingredientSummary}</ul>
+        <p>Total Price: ${this.props.price}</p>
+        <p>Continue Checkout?</p>
+        <Button clicked={this.props.purchaseCancelled} btnType={`Danger`}>
+          Cancel
+        </Button>
+        <Button btnType={`Success`} clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -3,19 +3,27 @@ import Backdrop from "./../Backdrop/Backdrop";
 
 import classes from "./Modal.module.css";
 
-const modal = (props) => (
-  <>
-    <Backdrop show={props.show} dropped={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.show ? "1" : "0",
-      }}
-    >
-      {props.children}
-    </div>
-  </>
-);
+class Modal extends React.Component {
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    return nextProps.show !== this.props.show;
+  }
 
-export default modal;
+  render() {
+    return (
+      <>
+        <Backdrop show={this.props.show} dropped={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? "translateY(0)" : "translateY(-100vh)",
+            opacity: this.props.show ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </>
+    );
+  }
+}
+
+export default Modal;
