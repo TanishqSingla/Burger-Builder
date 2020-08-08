c3162cb8419cfc22bb1c99722dd7a07befe3b039
TanishqSingla <tanishqsingla00@gmail.com>
2020-08-09 01:33:10 +0530

    Fixing typos

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -31,7 +31,7 @@ class Orders extends React.Component {
           <Order
             key={order.id}
             ingredients={order.ingredients}
-            price={order.pr}
+            price={order.price}
           />
         ))}
       </div>
