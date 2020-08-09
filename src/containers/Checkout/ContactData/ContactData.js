11798fbed17b6ffc4c3117c17895eeb466940749
TanishqSingla <tanishqsingla00@gmail.com>
2020-08-09 22:57:05 +0530

    Adding 2 way binding for input fields

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -80,6 +80,16 @@ class ContactData extends React.Component {
       });
   };
 
+  inputChangeHandler = (e, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm,
+    };
+    const updatedFormElement = { ...updatedOrderForm[inputIdentifier] };
+    updatedFormElement.value = e.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({ orderForm: updatedOrderForm });
+  };
+
   render() {
     const formElementsArray = [];
     for (let key in this.state.orderForm) {
@@ -96,6 +106,7 @@ class ContactData extends React.Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            changed={(event) => this.inputChangeHandler(event, formElement.id)}
           />
         ))}
         <Button btnType={"Success"} clicked={this.orderHandler}>
