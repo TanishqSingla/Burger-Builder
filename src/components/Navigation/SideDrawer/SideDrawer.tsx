da626c2ccfb53eda1132c9fb37f9b15d75526e2d
TanishqSingla <tanishqsingla00@gmail.com>
2021-06-03 21:53:00 +0530

    shifting layout files to tyepscript

--- src/components/Navigation/SideDrawer/SideDrawer.tsx
+++ src/components/Navigation/SideDrawer/SideDrawer.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Logo from "./../../Logo/Logo";
-import NavigationItems from "./../NavigationItems/NavigationItems";
-import BackDrop from "./../../UI/Backdrop/Backdrop";
+import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
+import BackDrop from "../../UI/Backdrop/Backdrop";
 import classes from "./SideDrawer.module.css";
 
-const sideDrawer = (props) => {
+const sideDrawer: React.FC = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
   if (props.open) {
     attachedClasses = [classes.SideDrawer, classes.Open];
